4a37aac9475959ed25d63438295a33d82db2a3bc
venutti <lvenutti@fi.uba.ar>
2023-05-11 18:47:20 -0300

    Fix: Problema con deploy en vercel
    Se copia el código original cuando se descarga el framework

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
-import type { LayoutProps } from "@/.next/types/app/layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +10,11 @@ export const metadata = {
   image: "/logo.png",
 };
 
-export default function RootLayout({ children }: LayoutProps) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="es-AR">
       <body className={inter.className}>
